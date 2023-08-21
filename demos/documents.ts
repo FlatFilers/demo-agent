a7d3eaa4549f2049e9230d0b8de94df50dda094c
Merit <merit@flatfile.io>
2023-08-21 09:09:19 -0700

    Documents must be inline
    Signed-off-by: Merit <merit@flatfile.io>

--- demos/documents.ts
+++ demos/documents.ts
@@ -1,6 +1,6 @@
 import api from "@flatfile/api";
 import { Client, FlatfileEvent, FlatfileListener } from "@flatfile/listener";
-import { readDocument, workbookConfig } from "../helpers";
+import { workbookConfig } from "../helpers";
 
 export default function flatfileEventListener(listener: Client) {
   listener.filter({ job: "space:configure" }, (configure: FlatfileListener) => {
@@ -15,12 +15,12 @@ export default function flatfileEventListener(listener: Client) {
 
           await api.documents.create(spaceId, {
             title: "About this Documents Demo",
-            body: readDocument("../examples-agent/documents/documents-1.md"),
+            body: `# About this Documents Demo\n\n---\n\nDocuments are ways of storing information right inside your Space. Provide guidance or reference for your customers without leaving Flatfile.\n\n## Making this space\n\nThis Space has been configured with multiple documents upon creation.\n\nHere's a look at the code that was used to create it:\n\n\`\`\`jsx\nimport api from "@flatfile/api";\nimport { Client, FlatfileEvent, FlatfileListener } from "@flatfile/listener";\nexport default function flatfileEventListener(listener: Client) {\n  listener.filter({ job: "space:configure" }, (configure: FlatfileListener) => {\n    configure.on(\n      "job:ready",\n      async ({ context: { spaceId, environmentId, jobId } }: FlatfileEvent) => {\n        try {\n          await api.jobs.ack(jobId, {\n            info: "Job started.",\n            progress: 10,\n          });\n          await api.documents.create(spaceId, {\n            title: "About this Documents Demo",\n            body: "Document text here.",\n          });\n          await api.documents.create(spaceId, {\n            title: "Configure multiple Documents",\n            body: "Document text here.",\n          });\n          await api.jobs.complete(jobId, {\n            outcome: {\n              message: "Job completed.",\n            },\n          });\n        } catch (error) {\n          await api.jobs.fail(jobId, {\n            outcome: {\n              message: "Job encountered an error.",\n            },\n          });\n        }\n      }\n    );\n  });\n}\n\`\`\`\`\n`,
           });
 
           await api.documents.create(spaceId, {
             title: "Configure multiple Documents",
-            body: readDocument("../examples-agent/documents/documents-1.md"),
+            body: `# Configure multiple Document\n\n---\n\nAs this example demonstrates, you many create as many Documents are you need.\n\n## Further documentation\n\nRead more about Documents [here](https://flatfile.com/docs/guides/documents).\n\n## Additional examples\n\n- [Dynamic Example](https://platform.flatfile.com/examples)\n- [Extractors Example](https://platform.flatfile.com/examples)\n- [Simple Example](https://platform.flatfile.com/examples)\n- [Theming Example](https://platform.flatfile.com/examples)\n`,
           });
 
           // @ts-ignore
