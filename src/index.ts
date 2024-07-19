9bfdfabb34068ac7b45b40eece177f10e615868b
Merit <11151764+meritmalling@users.noreply.github.com>
2024-07-19 08:35:21 -0700

    Fix listening ** (#51)
    Signed-off-by: Merit <merit@flatfile.io>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 "use strict";
-import api from "@flatfile/api";
-
 import actions from "./demos/actions";
 import connect from "./demos/connect";
 import data from "./demos/data";
@@ -35,25 +33,4 @@ export default function (listener: Client) {
   listener.namespace(["space:simple-demo"], simple)
   listener.namespace(["space:theming-demo"], theming)
   listener.namespace(["space:connect-demo"], connect)
-  // Temp workaround until we solve for new namespace pattern
-  // If namespace is not space:demo but label is, then run demo
-  listener.on("**", async (event) => {
-    const { context: { spaceId }, namespace } = event
-    const { data: { labels }} = await api.spaces.get(spaceId)
-    if (!namespace!.includes('space:actions-demo') && labels!.includes("actions-demo")) { actions(listener) }
-    if (!namespace!.includes('space:data-demo') && labels!.includes("data-demo")) { data(listener) }
-    if (!namespace!.includes('space:documents-demo') && labels!.includes("documents-demo")) { documents(listener) }
-    if (!namespace!.includes('space:dynamic-demo') && labels!.includes("dynamic-demo")) { dynamic(listener) }
-    if (!namespace!.includes('space:egress-demo') && (labels!.includes("egress-demo"))) { egress(listener) }
-    if (!namespace!.includes('space:extractor-demo') && labels!.includes("extractor-demo")) { extractor(listener) }
-    if (!namespace!.includes('space:headless-demo') && labels!.includes("headless-demo")) { headless(listener) }
-    if (!namespace!.includes('space:localization-demo') && labels!.includes("localization-demo")) { localization(listener) }
-    if (!namespace!.includes('space:metadata-demo') && labels!.includes("metadata-demo")) { metadata(listener) }
-    if (!namespace!.includes('space:namespace-demo') && labels!.includes("namespace-demo")) { namespaceListener(listener) }
-    if (!namespace!.includes('space:secrets-demo') && labels!.includes("secrets-demo")) { secrets(listener) }
-    if (!namespace!.includes('space:sidebar-demo') && labels!.includes("sidebar-demo")) { sidebar(listener) }
-    if (!namespace!.includes('space:simple-demo') && labels!.includes("simple-demo")) { simple(listener) }
-    if (!namespace!.includes('space:theming-demo') && labels!.includes("theming-demo")) { theming(listener) }
-    if (!namespace!.includes('space:connect-demo') && labels!.includes("connect-demo")) { connect(listener) }
-  })
 }
